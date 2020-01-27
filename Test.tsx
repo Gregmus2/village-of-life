96a431bfc1e9abba8407a8341a78b76a6963882c
Kostiantyn Osichenko <kostiantyn.osichenko@libify.com>
2020-01-27 12:25:51 +0200

    update tiles

--- Test.tsx
+++ Test.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="1800481A" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../../../../../Downloads/1800481A.png" width="512" height="512"/>
+<tileset version="1.2" tiledversion="1.3.2" name="1800481A" tilewidth="16" tileheight="16" tilecount="1600" columns="40">
+ <image source="grass.png" width="640" height="640"/>
 </tileset>
